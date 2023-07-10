a6f893b4f9ea517f6597785ef6068128fb9aba26
Marcos_MEDENOU <mmedenou@vippinterstis.com>
2023-07-10 19:22:33 +0200

    @updated

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- Header Logo Center -->
+    <header class="w-full text-gray-700 bg-white border-t border-gray-100 shadow-sm body-font">
+        <div class="mx-4 flex items-center justify-between p-5 mx-auto md:flex-row">
+
+            <nav class="flex gap-2 items-center text-base lg:w-2/5 md:ml-auto">
+                <span class="font-extralight">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32px" height="32px">
+                        <polygon fill="#0f518c"
+                            points="13.101,23 9,18.899 9,13.101 13.101,9 15,9 15,1.444 11.444,5 5,5 5,11.444 0.444,16 5,20.556 5,27 11.444,27 15,30.556 15,23" />
+                        <polygon fill="#ed0049"
+                            points="27,11.444 27,5 20.556,5 17,1.444 17,9 18.899,9 23,13.101 23,18.899 18.899,23 17,23 17,30.556 20.556,27 27,27 27,20.556 31.556,16" />
+                    </svg>
+                </span>
+                <a href="#" class="text-pink-700 mr-5 font-medium hover:text-gray-900">MasterCode</a>
+            </nav>
+
+            <form class="flex flex-row flex-wrap items-center ml-auto">
+                <div class="w-96 "><input placeholder="Search here" type="text"
+                        class="border-gray-200 shadow px-4 py-2 text-sm  w-full placeholder-blueGray-200 text-blueGray-700 relative bg-white rounded-lg outline-none focus:ring focus:ring-lightBlue-500 focus:ring-1 focus:border-lightBlue-500 border border-solid transition duration-200 ">
+                </div>
+            </form>
+
+            <div class="inline-flex items-center h-full ml-5 lg:w-2/5 lg:justify-end lg:ml-0">
+                <a href="#_" class="mr-5 font-medium hover:text-gray-900">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                    </svg>
+                </a>
+                <a href="#_" class="mr-5 font-medium hover:text-gray-900">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </a>
+                <a href="#_"
+                    class="px-2 py-2 text-xs font-bold text-white uppercase transition-all duration-150 rounded-full bg-[#0f518c] shadow outline-none active:bg-teal-600 hover:shadow-md focus:outline-none ease">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+                </a>
+            </div>
+    </div>
+</header></template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import MainLayout from '../layouts/MainLayout.vue';
+</script>
+
+<template>
+  <main>
+    <MainLayout />
+    
+  </main>
+</template>
